65a4f5d941116a5bd2e78729d42041499ae90f35
M17764017422 <31874069+M17764017422@users.noreply.github.com>
2021-01-22 01:34:56 +0800

    Update ci-build.sh

--- .ci/ci-build.sh
+++ .ci/ci-build.sh
@@ -8,7 +8,7 @@ make install
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:${PKG_CONFIG_PATH}
 cd ..    
 #sudo apt-get install python3 python3-pip ninja-build pkg-config cmake checkinstall -y
-export PATH=${USERPROFILE}\\.local\\bin:${PATH}
+export Path=${USERPROFILE}\\.local\\bin:${Path}
 python3 -m ensurepip
 python3 -m pip install --upgrade pip
 python3 -m pip install --upgrade --user setuptools
