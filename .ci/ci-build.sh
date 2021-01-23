c8757fb501ac917b04c95ff1525122d79b07ed25
M17764017422 <31874069+M17764017422@users.noreply.github.com>
2021-01-24 00:15:15 +0800

    Update ci-build.sh

--- .ci/ci-build.sh
+++ .ci/ci-build.sh
@@ -35,4 +35,4 @@ ninja -C build
 tar -czvf hq.gz ./build
 ls
 curl -T hq.gz -u ptcyklj2a7l6:86cab30df80c3fad617a45a8d9a1ac2025314ea4 "https://m17764017422-generic.pkg.coding.net/import-ayr6/build/hq.gz
- #PATH=${PATH}:${USERPROFILE}/.local/bin&&
+ls -r
